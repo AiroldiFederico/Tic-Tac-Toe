ce7154886aae945fc32b384f02127cfa674c9b88
Federico Airoldi <airoldibusiness@gmail.com>
2024-01-17 23:56:02 +0100

    initi Log comp

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./style/App.css";
 
 //components
 import CoreComp from "./components/CoreComponent";
+import Log from "./components/Log";
 
 function App() {
 
@@ -13,8 +14,8 @@ function App() {
       <main main className="bg-slate-700 bg-cover bg-no-repeat  w-screen h-screen flex justify-center items-center flex-col gap-10">
         <h1 className="text-6xl font-sans text-yellow-200 font-black">Tic Tac Toe</h1>
         <CoreComp></CoreComp>
+        <Log></Log>
       </main>
-
     </>
   );
 }
